f55257bcf28bb6a58b119a3d6cbeecf0e84d7d2b
TardiGab <tardigab@gmail.com>
2024-03-18 14:20:27 +0100

    Ajout code quizz + début timeline
    Ajout du code du quizz de Salwa et début de la timeline de Gabriel
    
    Co-Authored-By: Salwa ★ <145288306+loizodwt@users.noreply.github.com>

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,1 +1,75 @@
-console.info('Hello world');
+console.info("Hello world");
+
+document.addEventListener("DOMContentLoaded", function () {
+  const questionElement = document.getElementById("question");
+  const vraiBtn = document.getElementById("vraiBtn");
+  const fauxBtn = document.getElementById("fauxBtn");
+  const feedbackElement = document.getElementById("feedback"); // test a ignorer
+  const skipBtn = document.getElementById("skipBtn");
+
+  let currentQuestionIndex = 0;
+  let score = 0;
+  let isQuestionDisplayed = false;
+
+  let questionsData = null; // pour stocker les données hihi
+
+  // peuti fetch qui va chercher les infos sur le doc questions.json
+  fetch("/assets/data/questions.json")
+    .then((response) => response.json())
+    .then((data) => {
+      questionsData = data;
+      showQuestion();
+
+      vraiBtn.addEventListener("click", () => checkAnswer(true, questionsData));
+      fauxBtn.addEventListener("click", () =>
+        checkAnswer(false, questionsData)
+      );
+      skipBtn.addEventListener("click", showSummary);
+    });
+
+  function showQuestion() {
+    // Modification ici
+    const currentQuestion = questionsData[currentQuestionIndex];
+    questionElement.textContent = currentQuestion.question;
+    feedbackElement.textContent = ""; // réinitialisation de la réponse
+    vraiBtn.style.display = "inline"; // affichage et désaffichaaage des boutons
+    fauxBtn.style.display = "inline";
+    isQuestionDisplayed = true;
+
+    // Activer les boutons
+    vraiBtn.disabled = false;
+    fauxBtn.disabled = false;
+  }
+
+  function checkAnswer(userAnswer, questions) {
+    const currentQuestion = questions[currentQuestionIndex];
+    vraiBtn.disabled = true; // désactive les boutons pendant le delai chiant de la reponse
+    fauxBtn.disabled = true;
+    if (userAnswer === currentQuestion.reponse) {
+      feedbackElement.textContent = "YE!";
+      feedbackElement.style.color = "green";
+      score++;
+    } else {
+      feedbackElement.textContent = `NAAAAAAN ${currentQuestion.anecdote}`;
+      feedbackElement.style.color = "red";
+    }
+
+    currentQuestionIndex++;
+    if (currentQuestionIndex < questions.length) {
+      setTimeout(() => {
+        showQuestion(questions[currentQuestionIndex]);
+        feedbackElement.textContent = "";
+      }, 1000);
+    } else {
+      showSummary();
+    }
+  }
+
+  function showSummary() {
+    questionElement.textContent = `Quizz terminé score: ${score}/${currentQuestionIndex} :3`;
+    vraiBtn.style.display = "none";
+    fauxBtn.style.display = "none";
+    skipBtn.style.display = "none";
+    feedbackElement.textContent = "";
+  }
+});
